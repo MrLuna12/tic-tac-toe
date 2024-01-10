740735a08f58fae590f4755fede38fc53aac1201
Michael Luna <michaelluna244@gmail.com>
2024-01-09 21:14:32 -0600

    Finished the passing data through props section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// This is a component.
+// Components are used to render, manage, and update UI elements in the application
+function Square({ value }) {
+    return <button className="square">{value}</button>;
+
+}
+
+
+
+// 'export' makes the Square() accessible outside the file
+// 'default' tells other files using our code that it's the main funciton in the our file
+export default function Board() {
+  return (
+    <>
+        <div className="board-row">
+            <Square value="1" />
+            <Square value="2" />
+            <Square value="3" />
+        </div>
+        <div className="board-row">
+            <Square value="4" />
+            <Square value="5" />
+            <Square value="6" />
+        </div>
+        <div className="board-row">
+            <Square value="7" />
+            <Square value="8" />
+            <Square value="9" />
+        </div>
+    </>
+
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// This is the bridge between the components we create in the App.js file and the web browser
+import React, { StrictMode } from "react"; // React
+import { createRoot } from "react-dom/client"; // This is the react's library to talk to the broser (React DOM)
 import "./styles.css";
 
-import App from "./App";
+import App from "./App"; // This is our components
 
+// The code brings all the pieces together and puts them in the index.html files in the public folder
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
