e69932c4f2f800de3d94c9d64bd8b99a4e367edc
Michael Luna <michaelluna244@gmail.com>
2024-01-22 10:19:52 -0600

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -100,31 +100,4 @@ function calculateWinner(squares) {
       }
     }
     return winner;
-<<<<<<< HEAD
-  }
-
-  export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
-
-    function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
-    }
-
-
-    return (
-        <div className="game">
-            <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-            </div>
-            <div className="game-info">
-                <ol>{/*Todo*/}</ol>
-            </div>
-        </div>
-    );
-  }
-=======
 }
->>>>>>> cd8d9eff (updated ignored files)
